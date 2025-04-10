74c26c128b66f3cfaf621ed069fb2761fb57ada3
Gogo602 <gogomarosi61@gmail.com>
2025-04-10 09:48:15 +0300

    done with notification

--- src/components/Notification.jsx
+++ src/components/Notification.jsx
@@ -5,30 +5,82 @@ import { FaUserPlus } from "react-icons/fa6";
 
 const Notification = () => {
   return (
-            <div className='w-full mt-20 px-5 space-y-5'>
+            <div className='w-full mt-20 px-5 h-auto'>
                 <h1 className='text-xl bg-white dark:bg-gray-800 p-5'> Notifications</h1>
-                    <div className='p-5 text-sm flex items-center justify-between bg-white dark:bg-gray-800'>
-                        <div className='flex items-center space-x-3'>
-                            <FaCircleCheck className='w-6 h-6 text-green-500'/>
-                            <p>Alice Smith and David Brown have finished a session.</p>
+                    <div className='space-y-5 mt-5'>
+                        <h2 className='pt-2 px-2'>Today</h2>
+                        <div className='p-5 text-sm flex items-center justify-between bg-white dark:bg-gray-800'>
+                            <div className='flex items-center space-x-3'>
+                                <FaCircleCheck className='w-6 h-6 text-green-500'/>
+                                <p>Alice Smith and David Brown have finished a session.</p>
+                            </div>
+                            <p>9:30am</p>
                         </div>
-                        <p>9:30am</p>
+                        <div className='p-5 text-sm flex items-center justify-between bg-white dark:bg-gray-800'>
+                            <div className='flex items-center space-x-3'>
+                                <PiHandshakeFill className='w-6 h-6 text-orange-500'/>
+                                <p>Alice Smith and David Brown have finished a session.</p>
+                            </div>
+                            <p>11:30am</p>
+                        </div>
+                        <div className='p-5 text-sm flex items-center justify-between bg-white dark:bg-gray-800'>
+                            <div className='flex items-center space-x-3'>
+                                <FaUserPlus className='w-6 h-6 text-blue-500'/>
+                                <p>Alice Smith and David Brown have finished a session.</p>
+                            </div>
+                            <p>11:30am</p>
+                        </div>
                     </div>
-                <div className='pt-8 pl-5 text-sm flex items-center space-x-4'>
-                    <PiHandshakeFill className='w-6 h-6 text-orange-500'/>
-                    <div>
-                        <p>11:30am</p>
-                    <p>Alice Smith and David Brown have finished a session.</p>
+
+                    <div className='space-y-5 mt-5'>
+                        <h2 className='pt-2 px-2'>Yesterday</h2>
+                        <div className='p-5 text-sm flex items-center justify-between bg-white dark:bg-gray-800'>
+                            <div className='flex items-center space-x-3'>
+                                <FaCircleCheck className='w-6 h-6 text-green-500'/>
+                                <p>Alice Smith and David Brown have finished a session.</p>
+                            </div>
+                            <p>9:30am</p>
+                        </div>
+                        <div className='p-5 text-sm flex items-center justify-between bg-white dark:bg-gray-800'>
+                            <div className='flex items-center space-x-3'>
+                                <PiHandshakeFill className='w-6 h-6 text-orange-500'/>
+                                <p>Alice Smith and David Brown have finished a session.</p>
+                            </div>
+                            <p>11:30am</p>
+                        </div>
+                        <div className='p-5 text-sm flex items-center justify-between bg-white dark:bg-gray-800'>
+                            <div className='flex items-center space-x-3'>
+                                <FaUserPlus className='w-6 h-6 text-blue-500'/>
+                                <p>Alice Smith and David Brown have finished a session.</p>
+                            </div>
+                            <p>11:30am</p>
+                        </div>
                     </div>
-                </div>
-                
-                <div className='pt-8 pl-5 text-sm flex items-center space-x-4'>
-                    <FaUserPlus className='w-6 h-6 text-blue-500'/>
-                    <div>
-                        <p>11:30am</p>
-                    <p>Alice Smith and David Brown have finished a session.</p>
+          
+                    <div className='space-y-5 mt-5'>
+                        <h2 className='pt-2 px-2'>Old Message</h2>
+                        <div className='p-5 text-sm flex items-center justify-between bg-white dark:bg-gray-800'>
+                            <div className='flex items-center space-x-3'>
+                                <FaCircleCheck className='w-6 h-6 text-green-500'/>
+                                <p>Alice Smith and David Brown have finished a session.</p>
+                            </div>
+                            <p>9:30am</p>
+                        </div>
+                        <div className='p-5 text-sm flex items-center justify-between bg-white dark:bg-gray-800'>
+                            <div className='flex items-center space-x-3'>
+                                <PiHandshakeFill className='w-6 h-6 text-orange-500'/>
+                                <p>Alice Smith and David Brown have finished a session.</p>
+                            </div>
+                            <p>11:30am</p>
+                        </div>
+                        <div className='p-5 text-sm flex items-center justify-between bg-white dark:bg-gray-800'>
+                            <div className='flex items-center space-x-3'>
+                                <FaUserPlus className='w-6 h-6 text-blue-500'/>
+                                <p>Alice Smith and David Brown have finished a session.</p>
+                            </div>
+                            <p>11:30am</p>
+                        </div>
                     </div>
-                </div>
             </div>
          
 )
